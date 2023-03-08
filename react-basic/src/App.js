8cb1ef638fb69106c1baa42ca71976654fad85a9
felipeflourwears <A01274880@itesm.mx>
2023-03-07 22:28:41 -0600

    React Sintaxis JXS

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,14 +1,28 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let nombre="Felipe";
+  let auth=true;
+  let estaciones=["Primavera", "Verano", "Otoño", "Invierno"];
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <label htmlFor="nombre">nombre</label>
+        <h1>{nombre}</h1>
+        <input type='text' id='nombre'></input>
+        <p>{auth ? "El usuario esta logeado":"El usuario no esta logueado"} </p>
+        <p>{2+1}</p>
+        <ul>
+          {estaciones.map((el, index)=>(
+
+            <li key={index}>{el}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +33,8 @@ function App() {
         </a>
       </header>
     </div>
+    <div></div>
+  </>
   );
 }
 
