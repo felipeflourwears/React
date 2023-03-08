0faec9560cdf3bf23461020e79a10244e3a82994
felipeflourwears <A01274880@itesm.mx>
2023-03-07 23:30:36 -0600

    Componentes

---
+++ react-basic/src/components/Componente.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+const Componente=props=><h2>{props.msg}</h2>
+/* function Componente(props){
+    return <h2>{props.msg}</h2>
+} */
+/*Extenderse componente de REACT*/
+/* class Componente extends Component{
+    render(){
+        /* return <h2>Hola soy un componente</h2>
+        return <h2>{this.props.msg}</h2>;
+    }
+} */
+export default Componente;
